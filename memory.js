2db134ee31c3959029ad677c431433ba2911811c
Mquater <1119008738@qq.com>
2018-06-30 08:33:32 +0800

    111

--- memory.js
+++ memory.js
@@ -1,4 +1,5 @@
 var count=0;
+var totalCount=0;
 var first;
 var second;
 compareList=[];
@@ -7,35 +8,37 @@ $('.playbox .box').on('click',function(){
         console.log('请点击另一张卡牌');//如果点到已经翻开的牌
     }else{
      $(this).children().addClass('active');//点到未翻开的牌
-     console.log( $(this).children('.active'))
      count++;//点击次数
+     totalCount++;
      if(count===1){
         first=$(this).children().css('background-color');//记录第一张牌颜色
-        console.log(first);
-        console.log(count);
      }else{
         second=$(this).children().css('background-color');//记录第二张牌颜色
-        console.log(second);
-        console.log(count);
         //比较两张牌颜色
         if(first===second){
-            console.log(first===second)
-            return setTimeout(function(){
+            setTimeout(function(){
                 $('.playbox .box').children('.active').addClass('remove');
                 count=0;
             },500);//相同,class加上remove,延时1s
+            //判断游戏是否结束
+            if($('.playbox .box').children('.remove').length===10){
+                console.log(totalCount);
+                $('.playbox .box').children().removeClass('remove');
+                alert('游戏结束,您一共用了'+ totalCount +'步,请再来一局');
+            }
         }else{
-            return setTimeout(function(){
+            setTimeout(function(){
             $('.playbox .box').children('.active').removeClass('active');
             count=0;
             },5000);//不同,class移除active,延时5s
+            }        
         }
-        console.log(count);
+        
         }
-     }
 });
 $('.playagain').on('click',function(){
     $('.playbox .box').children().removeClass('remove');
     $('.playbox .box').children().removeClass('active');
     count=0;
+    totalCount=0;
 })
